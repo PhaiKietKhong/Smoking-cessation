8a62a5aabcabe2f8a66b3801aa091ab305e9ae16
hieubd <hieubdse172475@fpt.edu.vn>
2025-06-06 23:19:44 +0700

    implement community ui

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Login from "./components/Login/Login";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Regist from "./components/Regist/Regist";
 import Dashboard from "./components/Layout/Dashboard";
-
+import CommunityPage from "./components/Community/CommunityPage";
 import OnboadingPage from "./components/OnBoardingPage/OnboadingPage";
 import UserDashBoard from "./components/User/UserDashBoard";
 import FormerSmokeData from "./components/User/FormerSmokerData/FormerSmokeData";
@@ -25,6 +25,7 @@ function App() {
           <Route path="/onBoardingPage" element={<OnboadingPage />} />
           <Route path="/userDashBoard" element={<UserDashBoard />} />
           <Route path="/formersmokedata" element={<FormerSmokeData />} />
+          <Route path="/community" element={<CommunityPage />} />
         </Routes>
       </ThemeProvider>
     </Router>
