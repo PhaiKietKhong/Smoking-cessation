62627dc0aa425631f135b9a115c32164434e143b
Tran Phuong Nguyen Vu <vutpnse172225@fpt.edu.vn>
2025-06-14 09:59:59 +0700

    Add files via upload
    update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,39 @@ import "./App.css";
 import theme from "./Theme";
 import LandingPage from "./components/LandingPage/LandingPage";
 import Login from "./components/Login/Login";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Regist from "./components/Regist/Regist";
+import Dashboard from "./components/Layout/Dashboard";
+import CommunityPage from "./components/Community/CommunityPage";
+import OnboadingPage from "./components/OnBoardingPage/OnboadingPage";
+import UserDashBoard from "./components/User/UserDashBoard";
+import FormerSmokeData from "./components/User/FormerSmokerData/FormerSmokeData";
+import AdminDashboard from "./components/Admin/Dashboard";
+import UserManagement from "./components/Admin/UserManagement/UserManagement";
+import PackageManagement from "./components/Admin/PackageManagement/PackageManagement";
+
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <Routes>
+        <CssBaseline />        <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/adminDashboard" element={<AdminDashboard />} />
           <Route path="/regist" element={<Regist />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/onBoardingPage" element={<OnboadingPage />} />
+          <Route path="/userDashBoard" element={<UserDashBoard />} />
+          <Route path="/formersmokedata" element={<FormerSmokeData />} />
+          <Route path="/community" element={<CommunityPage />} />
+            {/* Admin Routes */}
+          <Route path="/adminDashboard" element={<AdminDashboard />} />
+          <Route path="/admin/users" element={<UserManagement />} />
+          <Route path="/admin/user-roles" element={<UserManagement />} />
+          <Route path="/admin/user-analytics" element={<UserManagement />} />
+          <Route path="/admin/packages" element={<PackageManagement />} />
         </Routes>
       </ThemeProvider>
-    </BrowserRouter>
+    </Router>
   );
 }
 
